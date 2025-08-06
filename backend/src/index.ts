58db6a2fd45c95bc84d7e431b5f835a6d26a5074
João Vitor Soares <joaovitorsoaresdemoraes59@gmail.com>
2025-08-05 21:30:52 -0300

    conectando ao mongo db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import cors from "cors"
-import dotenv from "dotenv"
+//import dotenv from "dotenv"
 import path from "path"
 import { connectDB } from "./config/db"
 import carRoutes from "./routes/carRoutes"
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+//dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+process.env.MONGO_URI = 'mongodb+srv://Robertin:123456@cluster0.aktpx.mongodb.net/'
+process.env.PORT = '3000'
 console.log("Valor de MONGO_URI lido:", process.env.MONGO_URI);
 
 const app = express()
